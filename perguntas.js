dbc2cf5bc89589d4b571b8a8a42e943d77294ce1
Ramon Diego de Oliveira Vieira <ramon.oliveira.vieira@escola.pr.gov.br>
2025-06-13 19:52:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Questionamento',
+    'Quem é o mais mentiroso entre as pessoas?',
+    'Todo mundo e voçê (acreditando que tem o poder da verdade? que comedia!)'
+)
+
+criaCartao(
+    'Questionamento',
+    'Quem é o menos humano na sociedade?',
+    'TODA A SOCIEDADE (acreditando que são superiores? que piada sem graça!)'
+)
+
+criaCartao(
+    'Questionamento',
+    'qual é a ilusão da vida?',
+    'TODO O ENTRETERIMERNTO (. . .)'
+)
+
+criaCartao(
+    'Questionamento',
+    'o que é a vida?',
+    '. . . um teatro . . .'
+)
+
+criaCartao(
+    'Questionamento',
+    'o que é o destino?', 
+    '. . . . . . u . . m . . . . S . . c . . r . . i . . p . . t. . . . . . .'
+)
